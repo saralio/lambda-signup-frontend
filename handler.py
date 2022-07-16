e407f022fd1b229109e911b2c1bde67a6b89dc20
mohit2152sharma <mohitlakshya@gmail.com>
2022-07-16 13:28:36 +0530

    updating sharing linkks

--- handler.py
+++ handler.py
@@ -1,5 +1,5 @@
-import json
 from saral_utils.utils.env import get_env_var, create_env_api_url
+from saral_utils.utils.frontend import ShareLinks
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -8,8 +8,10 @@ def signup_html(event, context):
     env = get_env_var('MY_ENV')
     region = get_env_var('MY_REGION')
     register_base_url = create_env_api_url(url='register.saral.club/register')
-
-    template_data = {'register_base_url': register_base_url}
+    sl = ShareLinks()
+    twitter_account_link = sl.twitter_account_link
+    navbar_links = {'twitter_account_link': twitter_account_link}
+    template_data = {'register_base_url': register_base_url, 'navbar_links': navbar_links}
 
     jinja_env = Environment(loader=FileSystemLoader('.'))
     with open('./templates/template.html', 'r') as file:
